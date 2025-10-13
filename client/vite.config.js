267d27c9ef3d8d19d556adc8e8911ff41ac41152
Govardhana Rao <govardhanarao.s@gmail.com>
2025-10-13 18:36:38 +0300

    mailersend configuration for reset mail

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+/*server: { proxy: { '/api': 'https://adminapplication-api.onrender.com' } }*/
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'https://adminapplication-api.onrender.com' } }
+  server: { proxy: { '/api': 'http://localhost:3000' } }
+
 })
